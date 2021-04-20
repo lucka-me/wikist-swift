6b50ebab8ce6d64304a5010b67fdb0db0f1e214e
lucka-me <i@lucka.moe>
2021-04-20 22:46:43 +0800

    Display icon in widget

--- Widget/Shared/ContributionsMatrixWidget.swift
+++ Widget/Shared/ContributionsMatrixWidget.swift
@@ -37,14 +37,25 @@ struct ContributionsMatrixWidget: Widget {
                 return
             }
             let now = Date()
+            let onFinished = { (favicon: Data?) in
+                DispatchQueue.main.async {
+                    let entryDate = Calendar.current.date(byAdding: .minute, value: 30, to: now) ?? now
+                    completion(.init(entries: [ .init(user, favicon: favicon, date: entryDate) ], policy: .atEnd))
+                }
+            }
             user.refresh { succeed in
                 if succeed {
                     Dia.shared.save()
                 }
-                DispatchQueue.main.async {
-                    let entryDate = Calendar.current.date(byAdding: .minute, value: 30, to: now) ?? now
-                    completion(.init(entries: [ .init(user, date: entryDate) ], policy: .atEnd))
+                guard
+                    let site = user.site,
+                    let url = URL(string: site.favicon)
+                else {
+                    onFinished(nil)
+                    return
                 }
+                let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad)
+                URLSession.shared.dataTask(with: request) { data, _, _ in onFinished(data) }.resume()
             }
         }
         
@@ -64,10 +75,12 @@ struct ContributionsMatrixWidget: Widget {
     struct Entry: TimelineEntry {
         let date: Date
         let user: WikiUser?
+        let favicon: Data?
         
-        init(_ user: WikiUser?, date: Date = .init()) {
+        init(_ user: WikiUser?, favicon: Data? = nil, date: Date = .init()) {
             self.date = date
             self.user = user
+            self.favicon = favicon
         }
     }
 
@@ -90,6 +103,12 @@ struct ContributionsMatrixWidget: Widget {
             VStack(alignment: .leading) {
                 if widgetFamily == .systemMedium {
                     HStack {
+                        if let image = Image(data: entry.favicon) {
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 16, height: 16)
+                        }
                         Text(user.username)
                         Spacer()
                         Text(user.site?.title ?? "")
@@ -119,7 +138,7 @@ struct ContributionsMatrixWidget: Widget {
 struct ContributionsMatrixWidget_Previews: PreviewProvider {
     static var previews: some View {
         ContributionsMatrixWidget.EntryView(entry: .init(Dia.preview.users().first!))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
     }
 }
 #endif
